4043979dc1f9db04053ff352d1d4d4160872eafc
Jose Pala <jlpala@gmail.com>
2020-11-14 16:05:19 +0000

    Remove all refrences to OAuth.

--- AreosaManagerAPI/Startup.cs
+++ AreosaManagerAPI/Startup.cs
@@ -10,16 +10,35 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Owin;
 
 namespace AreosaManagerAPI
 {
     public partial class Startup
     {
-        public void Configuration (IAppBuilder app)
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
-            ConfigureAuth(app);
+            Configuration = configuration;
+            Environment = environment;
         }
+
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment()) 
+                {
+                app.UseDeveloperExceptionPage();
+            }
+               
+        }
+
     }
 }
